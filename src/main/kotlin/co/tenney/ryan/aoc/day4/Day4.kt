6bbd0a0ed877c934377c6c844724f32f3c5365ca
Ryan Tenney <ryantenney@users.noreply.github.com>
2022-12-04 21:30:44 +0000

    Day 4

--- src/main/kotlin/co/tenney/ryan/aoc/day4/Day4.kt
+++ src/main/kotlin/co/tenney/ryan/aoc/day4/Day4.kt
@@ -4,16 +4,33 @@ import co.tenney.ryan.aoc.AocDay
 
 class Day4 : AocDay<Int>(4) {
 
-    override fun processInput(input: List<String>): Unit {
+    var ranges: List<List<ClosedRange<Int>>>? = null
 
+    override fun processInput(input: List<String>): Unit {
+        ranges = input.map({ assignments ->
+            assignments.split(',').map({ assignment ->
+                val range = assignment.split('-').map(Integer::parseInt)
+                range[0] .. range[1]
+            })
+        })
     }
 
     override fun part1(): Int {
-        return 0
+        return ranges!!.filter({ containsFully(it[0], it[1]) }).count()
     }
 
     override fun part2(): Int {
-        return 0
+        return ranges!!.filter({ overlaps(it[0], it[1]) }).count()
+    }
+
+    fun containsFully(a: ClosedRange<Int>, b: ClosedRange<Int>): Boolean {
+        return (b.start in a && b.endInclusive in a) ||
+                (a.start in b && a.endInclusive in b)
+    }
+
+    fun overlaps(a: ClosedRange<Int>, b: ClosedRange<Int>): Boolean {
+        return b.start in a || b.endInclusive in a ||
+                a.start in b || a.endInclusive in b
     }
 
 }
